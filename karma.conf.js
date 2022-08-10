2008d4fadd972da8dc215683af83eb22a2145f3b
nhat.phan <happeace304@gmail.com>
2022-08-10 13:49:28 +0700

    Add prettier

--- karma.conf.js
+++ karma.conf.js
@@ -27,10 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/portfolio'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
