39f9718440e9aaefb832d7c693cfd2bce4640da4
nhat.phan <happeace304@gmail.com>
2022-08-13 16:10:56 +0700

    Format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 
